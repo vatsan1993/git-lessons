08e3cc3af98d8155dc8d372484d53c274813da6c
Sozhavaram Srivatasan <sozhavaramsrivatsan@hotmail.com>
2026-04-09 09:30:37 +0530

    added more println methods to the Sample.java file

--- Sample.java
+++ Sample.java
@@ -1,5 +1,7 @@
 public class Sample{
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("How are you?");
+        System.out.println("have a good day");
     }
 }
